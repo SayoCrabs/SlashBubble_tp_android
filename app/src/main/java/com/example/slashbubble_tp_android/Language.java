622606ea8c24fd5dcf57a572fe178ca3b51017e2
Sandra <47884342+SayoCrabs@users.noreply.github.com>
2021-11-23 10:38:52 +0100

    Implementation traduction

---
+++ app/src/main/java/com/example/slashbubble_tp_android/Language.java
@@ -0,0 +1,50 @@
+package com.example.slashbubble_tp_android;
+
+public class Language {
+    private String name;
+    private boolean currentLanguage;
+
+    public Language(String name, boolean currentLanguage)
+    {
+        this.name = name;
+        this.currentLanguage = currentLanguage;
+    }
+
+    // region getter setter
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean getCurrentLanguage() {
+        return currentLanguage;
+    }
+
+    public void setCurrentLanguage(boolean currentLanguage) {
+        this.currentLanguage = currentLanguage;
+    }
+
+    // end region
+
+    // Text show in Spinner
+    @Override
+    public String toString()  {
+        return this.getName();
+    }
+}
+
+class LanguageDataUtils {
+
+
+    public static Language[] getLanguages()  {
+
+        Language fr = new Language(App.getAppResources().getString(R.string.fr_language), true);
+        Language en = new Language(App.getAppResources().getString(R.string.en_language), false);
+
+        return new Language[]{fr, en};
+    }
+}
--- app/src/main/java/com/example/slashbubble_tp_android/Settings.java
+++ app/src/main/java/com/example/slashbubble_tp_android/Settings.java
@@ -2,23 +2,47 @@ package com.example.slashbubble_tp_android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.AudioManager;
 import android.os.Bundle;
+import android.util.ArraySet;
 import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.LinearLayout;
 import android.widget.SeekBar;
+import android.widget.Spinner;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Settings extends AppCompatActivity {
     private static final String TAG = "Settings";
+
     LinearLayout layoutSound;
+
     SeekBar soundVolume;
     CheckBox sound;
+    AudioManager audioManager;
+
+    Spinner spinner_languages;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        Language[] languages = LanguageDataUtils.getLanguages();
+
+        spinner_languages = findViewById(R.id.spinnerLanguage);
+
+        ArrayAdapter<Language> adapter = new ArrayAdapter<Language>(this,
+                android.R.layout.simple_spinner_item,
+                languages);
+
+        this.spinner_languages.setAdapter(adapter);
 
         layoutSound = findViewById(R.id.layoutSound);
         soundVolume = findViewById(R.id.soundVolume);
@@ -26,10 +50,13 @@ public class Settings extends AppCompatActivity {
         sound.setOnCheckedChangeListener((buttonView, isChecked) -> {
             if(isChecked)
             {
-
+                soundVolume.setVisibility(View.GONE);
                 Log.v(TAG, "unChecked");
             }
-
+            else
+            {
+                soundVolume.setVisibility(View.VISIBLE);
+            }
         }
         );
 
